28bd18b5b8d44419f6e586204d1b3a549729144c
ABHINAV <117561934+Abhinav8639@users.noreply.github.com>
2025-10-29 20:54:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -141,5 +141,5 @@ app.post('/bookings', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Backend server running on http://localhost:${port}`);
+  console.log(`Backend server running on port ${port}`);
 });
